bca6c5ed70b55845f3b76514a63086ab1874ae6d
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2024-10-10 13:12:37 -0700

    Add files via upload

---
+++ mood-shop/data.js
@@ -0,0 +1,86 @@
+export const data = [
+    {
+      "id": 1,
+      "name": "happy",
+      "image": "./resources/images/happy_spongebob.gif",
+      "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
+      "price": 5.99
+    },
+    {
+      "id": 2,
+      "name": "sad",
+      "image": "./resources/images/sad_spongebob.gif",
+      "desc": "Tears are words that can't be explained.",
+      "price": 5.99
+    },
+    {
+      "id": 3,
+      "name": "angry",
+      "image": "./resources/images/angry_1.gif",
+      "desc": "Anger is one letter short of danger.",
+      "price": 5.99
+    },
+    {
+      "id": 4,
+      "name": "calm",
+      "image": "./resources/images/calm_woman.gif",
+      "desc": "Calmness is the cradle of power.",
+      "price": 5.99
+    },
+    {
+      "id": 5,
+      "name": "curious",
+      "image": "./resources/images/curious_owl.gif",
+      "desc": "Curiosity is the wick in the candle of learning.",
+      "price": 5.99
+    },
+    {
+      "id": 6,
+      "name": "disgust",
+      "image": "./resources/images/disgust_theoffice.gif",
+      "desc": "Disgust is an emotion we feel when faced with something repulsive.",
+      "price": 5.99
+    },
+    {
+      "id": 7,
+      "name": "sleepy",
+      "image": "./resources/images/sleepy_cat.gif",
+      "desc": "Sleep is the best meditation.",
+      "price": 5.99
+    },
+    {
+      "id": 8,
+      "name": "energetic",
+      "image": "./resources/images/energetic_puppy.gif",
+      "desc": "Energy and persistence conquer all things.",
+      "price": 5.99
+    },
+    {
+      "id": 9,
+      "name": "gloomy",
+      "image": "./resources/images/gloomy_girl.gif",
+      "desc": "Gloominess can cloud your heart, but there's always light at the end of the tunnel.",
+      "price": 5.99
+    },
+    {
+      "id": 10,
+      "name": "scared",
+      "image": "./resources/images/scared_spongebob.gif",
+      "desc": "Fear is the path to the dark side.",
+      "price": 5.99
+    },
+    {
+      "id": 11,
+      "name": "surprised",
+      "image": "./resources/images/surprised_2.gif",
+      "desc": "Surprises are the joy of life.",
+      "price": 5.99
+    },
+    {
+      "id": 12,
+      "name": "shy",
+      "image": "./resources/images/shy_dog.gif",
+      "desc": "Shyness is the shield against the unknown.",
+      "price": 5.99
+    }
+  ];
